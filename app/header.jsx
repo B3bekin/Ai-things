9246a78f2e3a3cb0f054dfef3c4ed8a9b9dd8a9e
B3bekin <92512370+B3bekin@users.noreply.github.com>
2023-05-16 12:10:15 +0000

    uachid

--- app/header.jsx
+++ app/header.jsx
@@ -1,32 +1,39 @@
 import React from "react";
 // import Logo from "../public/Logo.png"
+// import Glass from "../public/magnifying-glass.png"
 
 function Header() {
 
     return (
-        <div className="px-16">
-            <div className="flex justify-between items-center">
+        <div>
+            <div className="px-16">
+                <div className="flex justify-between items-center">
 
-               {/* <img src='/noiseless/public/Logo.png' height={64} width={199} alt="logo" /> */}
-               <h1 className="text-5xl">Kreate</h1>
+                    <div>
+                        <img src='/noiseless/public/Logo.png' height={64} width={199} alt="logo" />
+                    </div>
+                    {/* <h1 className="text-5xl">Kreate</h1> */}
 
-                <ul className="text-2xl flex gap-10 items-center">
-                    <li>Solutions</li>
-                    <li>Products</li>
-                    <li>Support</li>
-                    <li>News</li>
-                </ul>
+                    <ul className="text-2xl flex gap-10 items-center">
+                        <li>Solutions</li>
+                        <li>Products</li>
+                        <li>Support</li>
+                        <li>News</li>
+                    </ul>
 
-                <div className="text-2xl">
-                    <div className="bg-slate-600 text-white "> 
-                        <div className="min-w-[20rem]">
-                            dsklskdj
+                    <div className="text-2xl">
+                        <div className="bg-slate-600 text-white ">
+                            <div className="flex min-w-[12px] gap-5">
+                                <p>search</p>
+
+                                <img src="/noiseless/public/magnifying-glass.png" alt="" />
+                            </div>
                         </div>
                     </div>
+
                 </div>
 
             </div>
-
         </div>
     )
 }
